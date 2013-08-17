9e83e12f459e196558818ca67863ecdab483ca16
KingShaun <shaun@nmmu.ac.za>
2013-08-17 15:27:03 +0200

    Ext Link

--- js/main.js
+++ js/main.js
@@ -21,7 +21,11 @@ function checkPreAuth() {
 $(document).ready(function () {
 
     $(".MyJourneyLink").bind('click', function (event) {
-        var ref = window.open('myjourney.nmmu.ac.za', '_blank', 'location=yes')
+        //var ref = window.open('myjourney.nmmu.ac.za', '_blank', 'location=yes')
+        var ref = window.open('http://myjourney.nmmu.ac.za', '_blank', 'location=yes');
+        ref.addEventListener('loadstart', function () { alert('start: ' + event.url); });
+        ref.addEventListener('loadstop', function () { alert('stop: ' + event.url); });
+        ref.addEventListener('exit', function () { alert(event.type); });
     });
 
     //Listen for login form submit
