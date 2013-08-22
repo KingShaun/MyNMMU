f98c6793f1cd970afa63735866ea498748fa0087
KingShaun <shaun@nmmu.ac.za>
2013-08-22 13:34:55 +0200

    Maps popup

--- js/NMMUFunctions.js
+++ js/NMMUFunctions.js
@@ -308,14 +308,45 @@ $(document).on('pageshow', '#PageNCMap', function (e, data) {
     };
     // Define the map
     map = new google.maps.Map(document.getElementById("nc_map_canvas"), myOptions);
+
+    var contentString = '<div id="content">' +
+        '<div id="siteNotice">' +
+        '</div>' +
+        '<h1 id="firstHeading" class="firstHeading">North Campus</h1>' +
+        '<div id="bodyContent">' +
+        '<p><b>Uluru</b>, also referred to as <b>Ayers Rock</b>, is a large ' +
+        'sandstone rock formation in the southern part of the ' +
+        'Northern Territory, central Australia. It lies 335&#160;km (208&#160;mi) ' +
+        'south west of the nearest large town, Alice Springs; 450&#160;km ' +
+        '(280&#160;mi) by road. Kata Tjuta and Uluru are the two major ' +
+        'features of the Uluru - Kata Tjuta National Park. Uluru is ' +
+        'sacred to the Pitjantjatjara and Yankunytjatjara, the ' +
+        'Aboriginal people of the area. It has many springs, waterholes, ' +
+        'rock caves and ancient paintings. Uluru is listed as a World ' +
+        'Heritage Site.</p>' +
+        '<p>Attribution: Uluru, <a href="http://en.wikipedia.org/w/index.php?title=Uluru&oldid=297882194">' +
+        'http://en.wikipedia.org/w/index.php?title=Uluru</a> ' +
+        '(last visited June 22, 2009).</p>' +
+        '</div>' +
+        '</div>';
+
+    var infowindow = new google.maps.InfoWindow({
+        content: contentString,
+        maxWidth: 200
+    });
+
     // Add the marker
     var marker = new google.maps.Marker({
         position: latlngPos,
         map: map,
         title: "NMMU North Campus"
     });
+
+    google.maps.event.addListener(marker, 'click', function () {
+        infowindow.open(map, marker);
+    });
     // To add the marker to the map, call setMap();
-    marker.setMap(map);
+    //marker.setMap(map);
 });
 
 $(document).on('pageshow', '#PageSCMap', function (e, data) {
