fb1800c78442889308a3bdcc6025bd49aecc66cc
KingShaun <shaun@nmmu.ac.za>
2013-09-10 13:21:19 +0200

    test page

--- js/NMMUFunctions.js
+++ js/NMMUFunctions.js
@@ -1413,3 +1413,58 @@ function onPhotoURISuccess(imageURI) {
     //
     largeImage.src = imageURI;
 }
+
+/**
+ * Select picture from library
+ */
+function selectPicture() {
+    navigator.camera.getPicture(
+        function (uri) {
+            var img = document.getElementById('camera_image');
+            img.style.visibility = "visible";
+            img.style.display = "block";
+            img.src = uri;
+            document.getElementById('camera_status').innerHTML = "Success";
+        },
+        function (e) {
+            console.log("Error getting picture: " + e);
+            document.getElementById('camera_status').innerHTML = "Error getting picture.";
+        },
+        { quality: 50, destinationType: navigator.camera.DestinationType.FILE_URI, sourceType: navigator.camera.PictureSourceType.PHOTOLIBRARY });
+};
+
+/**
+ * Upload current picture
+ */
+function uploadPicture() {
+
+    // Get URI of picture to upload
+    var img = document.getElementById('camera_image');
+    var imageURI = img.src;
+    if (!imageURI || (img.style.display == "none")) {
+        document.getElementById('camera_status').innerHTML = "Take picture or select picture from library first.";
+        return;
+    }
+
+    // Verify server has been entered
+    //server = document.getElementById('serverUrl').value;
+    server = "http://webservices.nmmu.ac.za/mobileapp/Adverts.asmx?op=SaveImage";
+    if (server) {
+
+        // Specify transfer options
+        var options = new FileUploadOptions();
+        options.fileKey = "file";
+        options.fileName = imageURI.substr(imageURI.lastIndexOf('/') + 1);
+        options.mimeType = "image/jpeg";
+        options.chunkedMode = false;
+
+
+        // Transfer picture to server
+        var ft = new FileTransfer();
+        ft.upload(imageURI, server, function (r) {
+            document.getElementById('camera_status').innerHTML = "Upload successful: " + r.bytesSent + " bytes uploaded.";
+        }, function (error) {
+            document.getElementById('camera_status').innerHTML = "Upload failed: Code = " + error.code;
+        }, options);
+    }
+}
